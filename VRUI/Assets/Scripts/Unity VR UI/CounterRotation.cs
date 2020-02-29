f76aa870e17798359fe1e92670c1dab56c2c7e6a
Oliver Sheard <oliver.sheard@hotmail.co.uk>
2020-02-29 02:01:12 +0000

    Initial Commit

---
+++ VRUI/Assets/Scripts/Unity VR UI/CounterRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CounterRotation : MonoBehaviour
+{
+    [SerializeField] GameObject pointTo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {      
+        var pos = transform.parent.transform;
+
+        transform.rotation = Quaternion.FromToRotation(pos.position, pointTo.transform.position);
+    }
+}
---
+++ VRUI/Assets/Scripts/Unity VR UI/VRMouseInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRMouseInput : MonoBehaviour
+{
+    [SerializeField] private string inputAxisName;
+    [SerializeField] private float buttonThreshold = 0.5f;
+    [SerializeField] private Canvas[] affectedCanvases;
+    [SerializeField] private Camera[] affectedCameras;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var col = new int[7];
+
+        //col.
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var camera in affectedCameras)
+        {
+            //Physics.Raycast
+            Debug.DrawLine(transform.position, camera.WorldToScreenPoint(transform.position), Color.blue);
+
+            //affectedCanvases[0].
+        }
+        if(Input.GetButton(inputAxisName) || Input.GetAxis(inputAxisName) > buttonThreshold)
+        {
+            foreach (var canvas in affectedCanvases)
+            {
+                //Debug.DrawLine(transform.position, canvas.transform.position, Color.blue);
+            }
+
+            foreach (var cam in affectedCameras)
+            {
+                //cam.WorldToScreenPoint(transform.position);
+                
+
+                //Debug.DrawLine(transform.position, cam.WorldToScreenPoint(transform.position), Color.red);
+            }
+        }
+    }
+}
